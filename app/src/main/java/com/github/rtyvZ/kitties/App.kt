c83c8da7e9d34975b559110353a5eaf33be8089b
Z <zaicev.vasili@gmail.com>
2020-08-21 10:52:31 +0300

    add dependencies

---
+++ app/src/main/java/com/github/rtyvZ/kitties/App.kt
@@ -0,0 +1,10 @@
+package com.github.rtyvZ.kitties
+
+import android.app.Application
+
+class App :Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/github/rtyvZ/kitties/common/Api.kt
@@ -0,0 +1,4 @@
+package com.github.rtyvZ.kitties.common
+
+interface Api {
+}
