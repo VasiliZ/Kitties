7a2b8a4480af1b3f80d23717e0f4f3cabb589a2d
rtyvZ <vasilibapb@gmail.com>
2021-02-08 15:36:21 +0300

    add pagging library dependency

--- app/src/main/java/com/github/rtyvZ/kitties/extentions/SharedPreference.kt
+++ app/src/main/java/com/github/rtyvZ/kitties/extentions/SharedPreference.kt
@@ -1,6 +1,7 @@
 package com.github.rtyvZ.kitties.extentions
 
 import android.content.SharedPreferences
+import java.util.*
 
 private const val SESSION_USER_ID = "SESSION_USER_ID"
 
@@ -8,11 +9,11 @@ private const val SESSION_USER_ID = "SESSION_USER_ID"
 fun SharedPreferences.getUserId() = this.getString(SESSION_USER_ID, null)
 
 
-
 fun SharedPreferences.saveUserId(userId: String) {
     this.edit()
         .putString(SESSION_USER_ID, userId)
         .apply()
+    val set = LinkedHashSet<String>()
 }
 
 
