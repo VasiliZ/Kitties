73df04a65e27d7219c18f14b531a1c3e0d87f241
Z <zaicev.vasili@gmail.com>
2021-03-11 21:53:58 +0300

    review

--- app/src/main/java/com/github/rtyvZ/kitties/MainActivity.kt
+++ app/src/main/java/com/github/rtyvZ/kitties/MainActivity.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    //review
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
